50151362775a0dbbae04edffef506d5189538bc8
Johnny Bui <typngeek@aim.com>
2020-07-09 19:12:32 +0000

    improve documentation of fetchers

--- ftpvl/fetchers.py
+++ ftpvl/fetchers.py
@@ -9,7 +9,7 @@ import ftpvl.helpers as Helpers
 from ftpvl.evaluation import Evaluation
 
 
-class Fetcher():
+class Fetcher:
     """
     This is a superclass for all fetchers.
 
@@ -18,13 +18,11 @@ class Fetcher():
     """
 
     def __init__(self):
-        """Initializes the Fetcher superclass"""
         self._eval_id = None
 
     def _download(self) -> Any:
         """
-        Retrieves evaluation data over the internet and saves the data as
-        an instance variable.
+        Retrieves evaluation data over the internet and returns the data.
         """
         raise NotImplementedError
 
@@ -48,20 +46,24 @@ class HydraFetcher(Fetcher):
     Represents a downloader and preprocessor of test results from
     `hydra.vtr.tools`.
 
-    Attributes:
-        eval_num: A non-negative integer for the evaluation number to download,
-            with `0` being the latest evaluation
-        mapping: An optional dictionary mapping input column names to output
-            column names. If empty, does not remap the fetched data.
-        hydra_clock_names: An optional ordered list of strings used in finding
-            the actual frequency for each build result.
+    Parameters
+    ----------
+    eval_num : int, optional
+        A non-negative integer for the evaluation number to download,
+        with `0` being the latest evaluation, by default 0
+
+    mapping : dict, optional
+        A dictionary mapping input column names to output
+        column names, if needed for remapping, by default None
+
+    hydra_clock_names : list, optional
+        An optional ordered list of strings used in finding
+        the actual frequency for each build result, by default None
     """
 
-    def __init__(self, eval_num: int = 0, mapping: dict = None,
-                 hydra_clock_names: list = None) -> None:
-        """
-        Inits HydraFetcher with eval_num, mapping, and hydra_clock_names.
-        """
+    def __init__(
+        self, eval_num: int = 0, mapping: dict = None, hydra_clock_names: list = None
+    ) -> None:
         super().__init__()
         self.eval_num = eval_num
         self.mapping = mapping
@@ -72,27 +74,49 @@ class HydraFetcher(Fetcher):
         Fetches data from Hydra, returning a list of decoded meta.json dicts
         corresponding to the builds of the eval_num evaluation.
 
-        Overrides Fetcher._download().
+        Returns
+        -------
+        List[Dict]
+            A list of dictionaries that represent the decoded meta.json files
+            of each test result.
+
+        Raises
+        ------
+        ConnectionError
+            Raised if `hydra.vtr.tools` returns a non-200 status code when
+            fetching evals.
+
+        IndexError
+            Raised if the specified eval_num is invalid due to it being too
+            large.
+
+        ValueError
+            Raised if all builds in a given eval failed.
         """
         # get build numbers from eval_num
         resp = requests.get(
-            'https://hydra.vtr.tools/jobset/dusty/fpga-tool-perf/evals',
-            headers={'Content-Type': 'application/json'})
+            "https://hydra.vtr.tools/jobset/dusty/fpga-tool-perf/evals",
+            headers={"Content-Type": "application/json"},
+        )
         if resp.status_code != 200:
             raise ConnectionError("Unable to get evals from server.")
         evals_json = resp.json()
-        if self.eval_num >= len(evals_json['evals']):
+        if self.eval_num >= len(evals_json["evals"]):
             raise IndexError(f"Invalid eval_num: {self.eval_num}")
-        build_nums = evals_json['evals'][self.eval_num]['builds']
+        build_nums = evals_json["evals"][self.eval_num]["builds"]
 
         # collect the 'meta.json' build products
         data = []
         for build_num in build_nums:
             resp = requests.get(
-                f'https://hydra.vtr.tools/build/{build_num}/download/1/meta.json',
-                headers={'Content-Type': 'application/json'})
+                f"https://hydra.vtr.tools/build/{build_num}/download/1/meta.json",
+                headers={"Content-Type": "application/json"},
+            )
             if resp.status_code != 200:
-                print("Warning:", f"Unable to get build {build_num}")
+                print(
+                    "Warning:",
+                    f"Unable to get build {build_num}. It might have failed.",
+                )
                 continue
             try:
                 data += [resp.json()]
@@ -100,17 +124,15 @@ class HydraFetcher(Fetcher):
                 print("Warning:", f"Unable to decode build {build_num}")
 
         if len(data) == 0:
-            raise ValueError(f"Not able to fetch any builds from eval_num {self.eval_num}")
-    
-        self._eval_id = evals_json['evals'][self.eval_num]["id"]
+            raise ValueError(f"All builds from eval_num {self.eval_num} failed")
+
+        self._eval_id = evals_json["evals"][self.eval_num]["id"]
         return data
 
     def _preprocess(self, data: List[Dict]) -> pd.DataFrame:
         """
         Using data from _download(), processes and standardizes the data and
         returns a Pandas DataFrame.
-
-        Overrides Fetcher._preprocess().
         """
         flattened_data = [Helpers.flatten(x) for x in data]
 
@@ -122,35 +144,36 @@ class HydraFetcher(Fetcher):
             else:
                 for in_col_name, out_col_name in self.mapping.items():
                     processed_row[out_col_name] = row[in_col_name]
-            processed_row["freq"] = Helpers.get_actual_freq(row,
-                                                            self.hydra_clock_names)
+            processed_row["freq"] = Helpers.get_actual_freq(row, self.hydra_clock_names)
             processed_row.update(Helpers.get_versions(row))
             processed_data.append(processed_row)
 
-        return pd.DataFrame(processed_data).dropna(axis=1, how='all')
+        return pd.DataFrame(processed_data).dropna(axis=1, how="all")
 
 
 class JSONFetcher(Fetcher):
     """
-    Represents a loader and preprocessor of test results from local storage.
+    Represents a loader and preprocessor of test results from a JSON file.
 
-    Attributes:
-        path: A string file path pointing to the dataframe JSON file.
-        mapping: An optional dictionary mapping input column names to output
-            column names. If empty, does not remap the fetched data.
+    Parameters
+    ----------
+    path : str
+        A string file path pointing to the dataframe JSON file.
+
+    mapping : dict, optional
+        An optional dictionary mapping input column names to output
+        column names., by default None
     """
 
     def __init__(self, path: str, mapping: dict = None) -> None:
-        """
-        Inits FeatherFetcher with path and mapping.
-        """
+
         super().__init__()
         self.path = path
         self.mapping = mapping
 
     def _download(self) -> str:
         """
-        Retrieves evaluation data over the internet.
+        Returns the path of the JSON file.
         """
         return self.path
 
@@ -163,5 +186,6 @@ class JSONFetcher(Fetcher):
         if self.mapping is None:
             return loaded_df
         else:
-            return (loaded_df.filter(items=self.mapping.keys())
-                    .rename(columns=self.mapping))
+            return loaded_df.filter(items=self.mapping.keys()).rename(
+                columns=self.mapping
+            )
