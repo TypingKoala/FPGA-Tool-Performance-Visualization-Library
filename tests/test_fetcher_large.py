0d5464c63006ae08e299e2aba15968d584bab1b7
Johnny Bui <typngeek@aim.com>
2020-08-12 11:46:42 -0400

    Implement absolute eval_num HydraFetcher and Pagination support (#23)
    * add initial implementation and tests for absolute eval num fetching
    
    * Add support for hydrafetcher pagination
    
    * fixing redundant function signature and vars in hydrafetcher
    
    * fix self.eval_num confusion
    
    Co-authored-by: Johnny Bui <me@johnnybui.com>

--- tests/test_fetcher_large.py
+++ tests/test_fetcher_large.py
@@ -2,6 +2,7 @@
 
 """ Tests for Evaluation class """
 import unittest
+import pytest
 import json
 
 import pandas as pd
@@ -60,6 +61,8 @@ class TestHydraFetcherLarge(unittest.TestCase):
         """
         get_evaluation() should return an Evaluation corresponding to the real
         dataset and mapping.
+
+        Tests the use of relative eval_id.
         """
         with requests_mock.Mocker() as m:
             evals_fn = 'tests/sample_data/evals.large.json'
@@ -106,6 +109,78 @@ class TestHydraFetcherLarge(unittest.TestCase):
 
                     expected_num_rows = len(evals_json_decoded['evals'][eval_num]['builds'])
                     self.assertEqual(len(result.index), expected_num_rows)
+
+    def test_hydrafetcher_get_evaluation_absolute(self):
+        """
+        get_evaluation() should return an Evaluation corresponding to the real
+        dataset and mapping.
+
+        Tests the use of absolute eval_id.
+        """
+        with requests_mock.Mocker() as m:
+            evals_fn = 'tests/sample_data/evals.large.json'
+            evals_url = 'https://hydra.vtr.tools/jobset/dusty/fpga-tool-perf/evals'
+
+            build_fn = 'tests/sample_data/build.large.json'
+
+            meta_fn = 'tests/sample_data/meta.large.json'
+
+            # setup /evals request mock
+            evals_json_decoded = None
+            with open(evals_fn, "r") as f:
+                evals_json_encoded = f.read()
+                evals_json_decoded = json.loads(evals_json_encoded)
+                m.get(evals_url, text=evals_json_encoded)
+            
+            # read sample build response
+            build_json_encoded = None
+            with open(build_fn, "r") as f:
+                build_json_encoded = f.read()
+
+            # setup /build and /meta.json request mock
+            with open(meta_fn, "r") as f:
+                meta_json_encoded = f.read()
+                for eval_obj in evals_json_decoded["evals"]:
+                    for build_num in eval_obj["builds"]:
+                        # setup /build
+                        m.get(f'https://hydra.vtr.tools/build/{build_num}', text=build_json_encoded)
+                        # setup meta.json
+                        url = f'https://hydra.vtr.tools/build/{build_num}/download/5/meta.json'
+                        m.get(url, text=meta_json_encoded)
+
+            # map absolute eval_num to eval_data
+            eval_num_to_data = {}
+            for eval_data in evals_json_decoded["evals"]:
+                eval_num_to_data[eval_data["id"]] = eval_data
+
+            # run tests on different eval_num
+            for eval_num in [eval_data["id"] for eval_data in evals_json_decoded["evals"]]:
+                with self.subTest(eval_num=eval_num):
+                    result = None
+
+                    if len(eval_num_to_data[eval_num]["builds"]) == 0:
+                        with pytest.raises(ValueError): # expected exception
+                            result = HydraFetcher(
+                                project="dusty",
+                                jobset="fpga-tool-perf",
+                                eval_num=eval_num,
+                                absolute_eval_num=True).get_evaluation()
+                        continue # skip the rest of the test
+                    else:
+                        result = HydraFetcher(
+                            project="dusty",
+                            jobset="fpga-tool-perf",
+                            eval_num=eval_num,
+                            absolute_eval_num=True).get_evaluation()
+
+                    df = result.get_df()
+                    
+                    # find expected eval
+                    expected_eval_data = eval_num_to_data[eval_num]
+                    expected_num_rows = len(expected_eval_data["builds"])
+                    assert len(df.index) == expected_num_rows
+
+                    assert result.get_eval_id() == eval_num
 
 
 class TestJSONFetcherLarge(unittest.TestCase):
